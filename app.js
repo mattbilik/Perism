9a71d760ff09a571e72d024b18ecd42930116469
Matt <matthewsings5@gmail.com>
2018-08-02 01:16:53 -0400

    sad

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function() {
   var loginBtn = document.getElementById('btn-login');
   loginBtn.addEventListener('click', function(e) {
     var auth = document.getElementById('password').innerHTML
-    if(auth === '12345'){
+    if(auth == '12345'){
 
       window.location.href = "http://www.perism.net/survey";
 
