97acc017c519622c4ac716449db60dc7a854322d
Matt <matthewsings5@gmail.com>
2018-08-02 01:19:58 -0400

    sad

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-window.addEventListener('load', function() {
 
-  var webAuth = new auth0.WebAuth({
-    domain: 'perism.auth0.com',
-    clientID: 'zAMeDEMvV92aLChquVVnPCh2f7otpytC',
-    responseType: 'token id_token',
-    audience: 'https://perism.auth0.com/userinfo',
-    scope: 'openid',
-    redirectUri: window.location.href
-  });
 
   var loginBtn = document.getElementById('btn-login');
   loginBtn.addEventListener('click', function(e) {
